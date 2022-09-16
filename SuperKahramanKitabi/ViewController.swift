01c3fdfef0170d0d73e9312b79ae061c7b60c9a9
Hamza Oban <hamzaoban3@gmail.com>
2022-09-16 18:26:48 +0300

    Tableview data fetch, show, delete operationsTableview data fetch, show, delete operations

--- SuperKahramanKitabi/ViewController.swift
+++ SuperKahramanKitabi/ViewController.swift
@@ -11,22 +11,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     
     @IBOutlet weak var tableView: UITableView!
-    
+    var superHeroNames = [String]()
+    var superHeroImagesNames = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        superHeroNames.append("Batman")
+        superHeroNames.append("Spiderman")
+        superHeroNames.append("CaptanAmerica")
+        superHeroNames.append("IronMan")
+        superHeroNames.append("SuperMan")
+        
+        superHeroImagesNames.append("batman")
+        superHeroImagesNames.append("spiderman")
+        superHeroImagesNames.append("captanamerica")
+        superHeroImagesNames.append("ironman")
+        superHeroImagesNames.append("superman")
     }
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return superHeroNames.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = superHeroImagesNames[indexPath.row]
         return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            superHeroImagesNames.remove(at: indexPath.row)
+            superHeroNames.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
     }
     
 }
